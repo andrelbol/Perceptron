c850f7bcf4d7f7881196c9532c6c691aec9593c2
Andre Borges <andreluiborges1@gmail.com>
2018-05-02 21:31:08 -0300

    Tentei, mas não tá dando certo, desisto.

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import pandas
-import numpy
+import numpy as np
 
-def convertToArray(number):
+def convert_to_array(number):
     return {
         '1': [1, 0, 0],
         '2': [0, 1, 0],
         '3': [0, 0, 1]
     }.get(str(number))
 
-def getValues():
+def get_values_from_csv():
     data_frame = pandas.read_csv('data.csv')
-    return data_frame.iloc[:, 1:], data_frame.iloc[:, 0]
+    return data_frame.iloc[:, 0].values, data_frame.iloc[:, 1:].values
 
-def calculateOutput():
-
-
-def perceptron():
+def perceptron(max_iterations, alpha, X, D):
     t = 1
     E = 1
-    N = w.shape[1]
+    N = X.shape[1]
+    W = np.random.rand(N)
+    b = np.random.rand(N)
+    y = np.zeros(N)
+    e = np.zeros(N)
     print(N)
-    while(t<max_iterations and E > 0):
+    while(t < max_iterations and E > 0):
         E = 0
-        for i in range(1, N):
+        for i in range(0, N-1):
+            y[i] = W*X[i]+b[i]
+            e[i] = D[i] - y[i]
+            W = W+alpha*e[i]*x[i].T
+            b = b + alpha*e[i]
+            E = E + np.sum(e)
+        t += 1
+    return W
 
-X, d = getValues()
-perceptron(X, d)
+
+
+D, X = get_values_from_csv()
+max_iterations = 100
+alpha = 0.5
+result = perceptron(max_iterations, alpha, X, D)
+print(result)
